fbd34184b17e8432daaf68c3e1a8bc1e164ed765
YGenon <genonster@gmail.com>
2020-04-08 11:44:56 +0300

    start

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    
+    class Program
+    {   
+        private static object lockObject = new object();
+        //static int number = 0;
+        static long x = 1;
+        static long j = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {                
+                Thread thread = new Thread(Factorial);                
+                thread.Start();
+            }
+            Console.ReadKey();
+        }
+
+        static void Factorial()
+        {            
+            lock (lockObject) 
+            {                
+                j++;
+                Console.WriteLine( j + "! = " + j * x, x = j * x) ;
+            }
+            
+        }
+    }
+}
---
+++ SummNumber/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SummNumber
+{
+    class Program
+    {
+        private static object lockObject = new object();
+        
+        
+        static long j = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                Thread thread = new Thread(Factorial);
+                thread.Start();
+            }
+            Console.ReadKey();
+        }
+
+        static void Factorial()
+        {
+            lock (lockObject)
+            {
+                ++j;
+                long rezult = 0;
+                long count = 0;
+                Console.WriteLine();
+                Console.Write("Сумма чисел до числа " + (j-1) + " : ");
+
+                for (long i = 0; i < j; i++)
+                {
+                    rezult = rezult + i;
+                    count++;
+                    if (count < j) Console.Write(  i + " + " );                    
+                    if (count == j)Console.Write(i + " = " + rezult);
+                }
+                
+                //Console.WriteLine("Сумма до числа "+ j + " = " + (j + x), x = j + x);
+
+            }
+
+        }
+    }
+}
